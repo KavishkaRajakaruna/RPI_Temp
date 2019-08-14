929265a05c4ca1cac0b46862923650fcdcd9aa15
Kavishka Hirushan <rmkhrajakaruna@std.appsc.sab.ac.lk>
2019-08-14 15:32:08 +0100

    Fully fuctional With DB Update

--- monitor-temp.py
+++ monitor-temp.py
@@ -1,6 +1,24 @@
 import os
 import time
-import mysql.connector 
+import mysql.connector
+import RPi.GPIO as GPIO
+
+in1 = 24
+in2 = 23
+en = 25
+temp1=1
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(in1,GPIO.OUT)
+GPIO.setup(in2,GPIO.OUT)
+GPIO.setup(en,GPIO.OUT)
+
+GPIO.output(in1, GPIO.LOW)
+GPIO.output(in2,GPIO.LOW)
+p=GPIO.PWM(en,1000)
+
+p.start(15) 
+temp1=0
 
 db = mysql.connector.connect(
 	host="localhost",
@@ -9,17 +27,37 @@ db = mysql.connector.connect(
 	database= "temperature"
 	)
 mycursor=db.cursor()
-sql ="""INSERT INTO Temp_log (Temperature) VALUES (%s)"""
+sql ="""INSERT INTO Temp_log (Temperature,RPM) VALUES (%s,%s)"""
 
-temp1=0
 def measure_temp():
 	temp  = os.popen("vcgencmd measure_temp").readline()
 	return (temp.replace("temp=","").replace("'C\n",""))
 
 while True:
-	time.sleep(1)
-	temp1 =measure_temp() 
+	
+	temp1 =measure_temp()
+	temp2=float(temp1)
 	print(temp1)
-	sql_temp=(temp1,)
+	
+	
+	if 35>temp2>30:
+                print("run")
+                GPIO.output(in1,GPIO.HIGH)
+                GPIO.output(in2,GPIO.LOW)
+		speed =15
+        elif 40>temp2>35:
+		GPIO.output(in1,GPIO.HIGH)
+		GPIO.output(in2,GPIO.LOW)
+		p.ChangeDutyCycle(50)
+		speed = 50
+        elif temp2>40:
+		GPIO.output(in1,GPIO.HIGH)
+		GPIO.output(in2,GPIO.LOW)
+		p.ChangeDutyCycle(80)
+		speed = 80
+	
+	sql_temp = (temp1,speed,)
 	mycursor.execute(sql,sql_temp)
 	db.commit()
+	time.sleep(3)
+GPIO.cleanup()
