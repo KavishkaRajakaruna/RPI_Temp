80cc5e8161fc7c9df3965bb6d958f8f7e17f39f4
Kavishka Hirushan <rmkhrajakaruna@std.appsc.sab.ac.lk>
2019-08-14 14:34:03 +0100

    Temperature logging to a DB

--- monitor-temp.py
+++ monitor-temp.py
@@ -1,10 +1,25 @@
 import os
-import time 
+import time
+import mysql.connector 
 
+db = mysql.connector.connect(
+	host="localhost",
+	user="temp_logger",
+	passwd="abc@123",
+	database= "temperature"
+	)
+mycursor=db.cursor()
+sql ="""INSERT INTO Temp_log (Temperature) VALUES (%s)"""
+
+temp1=0
 def measure_temp():
 	temp  = os.popen("vcgencmd measure_temp").readline()
-	return (temp.replace("temp=",""))
+	return (temp.replace("temp=","").replace("'C\n",""))
 
 while True:
-	print(measure_temp())
 	time.sleep(1)
+	temp1 =measure_temp() 
+	print(temp1)
+	sql_temp=(temp1,)
+	mycursor.execute(sql,sql_temp)
+	db.commit()
